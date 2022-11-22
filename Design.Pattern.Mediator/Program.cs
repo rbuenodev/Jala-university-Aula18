7adb5c60d185dcbe50686d6ad3b4740bfce918e3
richard augusto bueno <richard_bueno@ymail.com>
2022-11-22 18:32:55 -0300

    adding refactoring

--- Design.Pattern.Mediator/Program.cs
+++ Design.Pattern.Mediator/Program.cs
@@ -1,9 +1,12 @@
 using Design.Pattern.Command.Api;
 using Design.Pattern.Mediator;
+using Design.Pattern.Mediator.ChatRoomExample;
+using Design.Pattern.Mediator.ChatRoomExample.Commands;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 services.AddMediator(typeof(Program));
+services.AddMemoryCache();
 
 var provider = services.BuildServiceProvider();
 var mediator = provider.GetRequiredService<IMediator>();
@@ -11,6 +14,16 @@ var mediator = provider.GetRequiredService<IMediator>();
 Console.WriteLine(mediator.Send(new CollegueA()));
 Console.WriteLine(mediator.Send(new CollegueB()));
 Console.WriteLine(mediator.Send(new CollegueC()));
+
+var from = new Developer("Dev");
+var to = new Tester("Tester");
+var message = "testing messages";
+mediator.Send(new RegisterTeamMemberCommand() { TeamMember = from });
+mediator.Send(new RegisterTeamMemberCommand() { TeamMember = to });
+mediator.Send(new MessageCommand() { From = from, To = to, Message = message });
+
+
+
 // var receivers = new Dictionary<Type, Type>()
 // {
 //     {typeof(CollegueA), typeof(CollegueHandler)},
